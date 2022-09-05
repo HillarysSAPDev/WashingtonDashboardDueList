7407274f9c41ff37144a331fdf76d801b47cb037
Gomathy Natarajan <gomathy.natarajan@hillarys.co.uk>
2022-09-05 10:06:13 +0000

    Due list report issue based on date.

--- webapp/controller/DueList.controller.js
+++ webapp/controller/DueList.controller.js
@@ -7,6 +7,7 @@ sap.ui.define([
 	var that;
 	var cellList = [];
 	var dueDate;
+	var datetoDay;
 	return Controller.extend("WashingtonDashboardDueList.WashingtonDashboardDueList.controller.DueList", {
 
 		onInit: function () {
@@ -14,45 +15,12 @@ sap.ui.define([
 
 			var oModel = new ODataModel("/sap/opu/odata/sap/ZPROD_ORD_DET_SRV/");
 			oView.setModel(oModel);
-
 		},
 
 		onInitSmartFilterBarExtension: function (event) {
-			// var aFilters = [];
-
-			// var filter;
-			// var cellList = [];
-
-			// var userID = sap.ushell.Container.getService("UserInfo").getId();
-
-			// filter = new sap.ui.model.Filter("Uname", sap.ui.model.FilterOperator.EQ, userID);
-			// aFilters.push(filter);
 
 			var smartFilterView = this.getView().byId("smartFilterBar");
 			var schFinishDate = smartFilterView.getControlByKey("Scheduledfinish");
-
-			// var cellName = smartFilterView.getControlByKey("Cellname");
-
-			// var oCellDataModel = this.getOwnerComponent().getModel("WDUserAdmin");
-			// oCellDataModel.read("/ZUSER_CELL_LISTSet", {
-			// 	filters: aFilters,
-
-			// 	success: function (oData, oResponse) {
-			// 		var results = oData.results;
-
-			// 		results.forEach(function (cellname) {
-			// 			var newToken = new Token({
-			// 				key: cellname.Arbpl,
-			// 				text: cellname.Arbpl
-			// 			});
-			// 			cellList.push(newToken);
-			// 		});
-			// 		cellName.setTokens(cellList);
-			// 	},
-			// 	error: function (oError) {
-			// 		console.log(oError);
-			// 	}
-			// });
 
 			// Get the user type array from the session storage
 			var oStorage = jQuery.sap.storage(jQuery.sap.storage.Type.session);
@@ -63,7 +31,7 @@ sap.ui.define([
 			});
 			if (days !== "-1" && days !== "0") {
 				var Day = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() + parseFloat(days));
-				var datetoDay = dateFormat.format(Day);
+				datetoDay = dateFormat.format(Day);
 			} else if (days == "0") {
 				var datetoDay = dateFormat.format(new Date());
 			} else if (days == "-1") {
@@ -80,7 +48,7 @@ sap.ui.define([
 			schFinishDate.setValue(datetoDay);
 		},
 		onBeforeRebindTable: function (oSource) {
-			if (dueDate !== undefined) {
+			if (datetoDay !== undefined) {
 				var binding = oSource.getParameter("bindingParams");
 				var oFilter = new sap.ui.model.Filter("Scheduledfinish", sap.ui.model.FilterOperator.LT, dueDate);
 				binding.filters.push(oFilter);
